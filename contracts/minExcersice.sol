1c301e5ca0b6a300e639d8d4353b51a98b5cd10e
sadikbaba <4821saddikhady@gmail.com>
2026-02-09 14:17:13 +0100

    min excercise

---
+++ contracts/minExcersice.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Min {
+    
+    // source https://github.com/Vectorized/solady/blob/dcdfab80f4e6cb9ac35c91610b2a2ec42689ec79/src/utils/FixedPointMathLib.sol#L1100
+    // i make it external for simplicity
+        function min(uint256 x, uint256 y) external pure returns (uint256 z) {
+      
+        assembly {
+            z := xor(x, mul(xor(x, y), lt(y, x)))
+        }
+    }
+}
