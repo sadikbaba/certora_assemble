f22fcbda5d38f64d360a7405c4b40d93c516a1c6
sadikbaba <4821saddikhady@gmail.com>
2026-03-11 23:01:12 +0100

    Update example.sol

--- example.sol
+++ example.sol
@@ -1,1 +1,33 @@
-no
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract RestrictedVault {
+    address public allowedSender;
+    uint256 public totalDeposits;
+
+    constructor(address _allowedSender) {
+        allowedSender = _allowedSender;
+    }
+
+    // Developer tries to restrict who can send ETH
+    receive() external payable {
+        require(msg.sender == allowedSender, "Not allowed to send ETH");
+        totalDeposits += msg.value;
+    }
+
+    fallback() external payable {
+        require(msg.sender == allowedSender, "Not allowed to send ETH");
+        totalDeposits += msg.value;
+    }
+
+    function withdraw(address payable to, uint256 amount) external {
+        require(msg.sender == allowedSender, "Only controller");
+        require(address(this).balance >= amount, "Insufficient balance");
+
+        to.transfer(amount);
+    }
+
+    function vaultBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+}
